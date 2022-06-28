2cb476e64f6f9189348dc1984cda9ee312072ccf
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-06-28 03:03:03 -0700

    tasks

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+n = 97
+while n <= 122:
+    print(chr(n), end="")
+    n = n + 1
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for a in "abcdfghijklmnoqrstuvxyz":
+    print(f'{a}', end="")
