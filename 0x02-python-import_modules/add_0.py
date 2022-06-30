6d1eb5ee477e0ac8cffe94e1f37661e092552bd2
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-06-30 03:40:08 -0700

    task 0

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        the return value. a + b
+    """
+    return (a + b)
