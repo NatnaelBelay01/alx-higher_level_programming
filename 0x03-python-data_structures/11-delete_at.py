5676b5440db26ae6c5fe631f8cafe2ad870ff462
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-07-02 06:09:01 -0700

    task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    count = 0
+    for i in my_list:
+        count += 1
+    if idx < 0 or idx > count - 1:
+        return my_list
+    del my_list[idx]
+    return my_list
