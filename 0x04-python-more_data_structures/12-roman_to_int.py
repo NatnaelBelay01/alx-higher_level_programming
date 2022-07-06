0ad04c5c37f5a26d0ea81aa280ac14c2a1a36658
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-07-06 11:52:01 -0700

    fixes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     num, i = 0, 0
-    if not roman_string or type(roman_string) != str:
+    let = roman_string
+    if not let or type(let) != str:
         return 0
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for j in roman_string:
+    for j in let:
         if val.get(j, 0) == 0:
             return 0
-    while i < len(roman_string):
+    while i < len(let):
         if num == 0:
-            num = val[roman_string[i]]
+            num = val[let[i]]
             i += 1
             continue
-        elif i <= 2 and val[roman_string[i]] == val[roman_string[i - 1]]:
-            if val[roman_string[i]] and val[roman_string[i - 2]] in (10, 100, 1000):
-                num *= val[roman_string[i]]
+        elif i <= 2 and val[let[i]] == val[let[i - 1]]:
+            if val[let[i]] and val[let[i - 2]] in (10, 100, 1000):
+                num *= val[let[i]]
             else:
-                num += val[roman_string[i]]
+                num += val[let[i]]
             i += 1
             continue
-        elif val[roman_string[i]] > val[roman_string[i - 1]]:
-            num = val[roman_string[i]] - num
+        elif val[let[i]] > val[let[i - 1]]:
+            num = val[let[i]] - num
             i += 1
             continue
-        elif val[roman_string[i]] <= val[roman_string[i - 1]]:
-            num += val[roman_string[i]]
+        elif val[let[i]] <= val[let[i - 1]]:
+            num += val[let[i]]
             i += 1
             continue
     return num
