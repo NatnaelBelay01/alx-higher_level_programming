cb627f1dfe3374b4edb4f9f4ef6e329a8d9ff1b4
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-07-06 05:09:07 -0700

    fixes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     prev = None
     res = 0
     for i in my_list:
-        if i != prev or prev == None:
+        if i != prev:
             prev = i
             res += prev
     return res
