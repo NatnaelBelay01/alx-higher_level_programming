c3cf5dc221bec55e659c860dd07570dbd23fb2ab
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-08-01 05:12:40 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function to check inheretance"""
+
+
+def inherits_from(obj, a_class):
+    st1, st2 = set(dir(obj)), set(dir(a_class))
+    return st1 | st2 == st1 and st1 != st2
