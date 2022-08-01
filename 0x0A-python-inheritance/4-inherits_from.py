ff7c84c20596f0ffb8631d29a9665a7c78e4b225
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-08-01 05:16:16 -0700

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,5 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ defines the function """
-    st1, st2 = set(dir(obj)), set(dir(a_class))
-    return st1 | st2 == st1 and st1 != st2
+    return issubclass(type(obj), a_class) and type(obj) != a_class
