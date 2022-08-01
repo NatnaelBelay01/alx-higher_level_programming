f9222a57d15a95c3d4fea3fa954d88194cfed6ad
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-08-01 05:26:42 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class BaseGeommetry"""
+
+
+class BaseGeometry():
+    """defines the class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
