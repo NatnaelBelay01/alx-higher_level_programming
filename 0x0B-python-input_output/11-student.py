d80d25803b2ca39106bd44a033b89461d6dd5688
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-08-02 07:21:48 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A class that defines a student obj"""
+
+
+class Student():
+    """defiens the class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrives dictionary representation of Student"""
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """a method that replaes all the attributes"""
+        for x in json:
+            self.__dict__.update({x:jason[x]})
