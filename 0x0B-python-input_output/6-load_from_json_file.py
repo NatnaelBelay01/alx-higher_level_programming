03e7c4137af4d199ade53e77ea5ff4966bffbe00
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-08-02 04:54:40 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that returns an object from a jason file"""
+import json
+
+
+def load_from_json_file(filename):
+    """defines the function"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.loads(f.read())
